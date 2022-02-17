ad35bf36101c5883a804705af14800ff8b550bea
Katlen Abreu <katleeen.abreu18@gmail.com>
2022-02-16 21:00:16 -0300

    Primeiros exercicios

---
+++ 1001/soma.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int A, B, X;
+
+                A = int.Parse(Console.ReadLine());
+                B = int.Parse(Console.ReadLine());
+
+            X = A + B;
+
+            Console.WriteLine("X = " + X);
+
+        }
+    }
+}
