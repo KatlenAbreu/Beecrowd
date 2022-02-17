2ecddd142bb63dadb47a4c33b86dce8ec6f0d94e
Katlen Abreu <katleeen.abreu18@gmail.com>
2022-02-16 21:09:19 -0300

    Produto simples

---
+++ 1004/produto_simples.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace curso
+{
+    class Program
+    {
+        static void Main(String[] argos)
+        {
+            int A, B, PROD;
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            PROD = A * B;
+            Console.WriteLine("PROD = " + PROD);
+        }
+    }
+}
