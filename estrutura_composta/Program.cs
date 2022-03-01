14b7c9883fd73543aa0a549a2401b99429429e94
Katlen Rodrigues <Katlen Rodrigues@DESKTOP-N1P4VVE>
2022-03-01 20:16:28 -0300

    Adicionar arquivos de projeto.

---
+++ estrutura_composta/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        int hora;
+        Console.WriteLine("Que horas são?");
+        hora = int.Parse(Console.ReadLine());
+
+        if (hora <= 12)
+        {
+            Console.WriteLine("Bom dia!");
+        }
+        else if (hora <= 18)
+                Console.WriteLine("Boa tarde!");
+
+            else
+            {
+                Console.WriteLine("Boa noite!");
+            }
+            Console.ReadLine();
+
+    }
+}
+
+    
